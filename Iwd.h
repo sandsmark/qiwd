a1959770163983cc6c8d4e90b15d677502edb9ba
Martin T. H. Sandsmark <martin.sandsmark@kde.org>
2019-12-06 23:01:51 +0100

    start on implementing the API for the signal level and auth agents

--- Iwd.h
+++ Iwd.h
@@ -7,6 +7,8 @@
 // Let's not get too carried away
 using namespace net::connman;
 
+class SignalLevelAgent;
+
 class Iwd : public QObject
 {
     Q_OBJECT
@@ -32,12 +34,22 @@ signals:
     void visibleNetworkRemoved(const QString &name);
     void deviceRemoved(const QString &name);
 
+    void signalLevelChanged(const QString &networkName, int level);
+
 private slots:
     void onManagedObjectsReceived(QDBusPendingCallWatcher *watcher);
     void onManagedObjectRemoved(const QDBusObjectPath &object_path, const QStringList &interfaces);
     void onManagedObjectAdded(const QDBusObjectPath &objectPath, const ManagedObject &object);
 
 private:
+    friend class SignalLevelAgent;
+    void onSignalLevelChanged(const QDBusObjectPath &object, uint8_t signalLevel) {
+        if (!m_networks.contains(object)) {
+            return;
+        }
+        emit signalLevelChanged(m_networks[object]->name(), signalLevel);
+    }
+
     template<class T>
     T* addObject(const QDBusObjectPath &path, QMap<QDBusObjectPath, QPointer<T>> &map, const QVariantMap &props) {
         if (map.contains(path)) {
@@ -64,5 +76,109 @@ private:
     QMap<QDBusObjectPath, QPointer<iwd::Station>> m_stations;
     QMap<QDBusObjectPath, QPointer<iwd::SimpleConfiguration>> m_wps;
 };
+
+class SignalLevelAgent : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "net.connman.iwd.SignalLevelAgent")
+
+public:
+    SignalLevelAgent(Iwd *parent);
+
+public slots:
+    QDBusVariant Changed(const QDBusObjectPath &object, uint8_t level) {
+        m_iwd->onSignalLevelChanged(object, level);
+        return QDBusVariant("");
+    }
+    Q_NOREPLY void Release(const QDBusObjectPath &object) {
+        qDebug() << "deleting signal level listener" << object.path();
+        deleteLater();
+    }
+
+    Iwd *m_iwd;
+};
+
+class AuthAgent : public QDBusAbstractAdaptor
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "net.connman.iwd.Agent")
+
+public:
+    AuthAgent(Iwd *parent);
+
+public slots:
+    /**
+     * This method gets called when the service daemon
+     * unregisters the agent. An agent can use it to do
+     * cleanup tasks. There is no need to unregister the
+     * agent, because when this method gets called it has
+     * already been unregistered.
+     */
+    Q_NOREPLY void Release(const QDBusObjectPath &object) {
+        qDebug() << "deleting auth agent" << object.path();
+        deleteLater();
+    }
+
+    /**
+     * This method gets called when trying to connect to a
+     * network and passphrase is required.
+     *
+     * Possible Errors: net.connman.iwd.Agent.Error.Canceled
+     */
+    QDBusVariant RequestPassphrase(const QDBusObjectPath &network) {
+        return QDBusVariant("TODO");
+    }
+
+    /**
+     * This method gets called when connecting to
+     * a network that requires authentication using a
+     * locally-stored encrypted private key file, to
+     * obtain that private key's encryption passphrase.
+     *
+     * Possible Errors: net.connman.iwd.Agent.Error.Canceled
+     *
+     * @return a string
+     */
+    QDBusVariant RequestPrivateKeyPassphrase(const QDBusObjectPath &network) {
+        return QDBusVariant("TODO");
+    }
+
+    /**
+     * This method gets called when connecting to
+     * a network that requires authentication using a
+     * user name and password.
+     *
+     * Possible Errors: net.connman.iwd.Agent.Error.Canceled
+     *
+     * @return two strings
+     */
+    QDBusVariant RequestUserNameAndPassword(const QDBusObjectPath &network) {
+        return QDBusVariant(QStringList({"TODO", "TODO"}));
+    }
+
+    /**
+     * This method gets called when connecting to
+     * a network that requires authentication with a
+     * user password.  The user name is optionally passed
+     * in the parameter.
+     */
+    QDBusVariant RequestUserPassword(const QDBusObjectPath &network, const QString &username) {
+        return QDBusVariant("TODO");
+    }
+
+    /**
+     * This method gets called to indicate that the agent
+     * request failed before a reply was returned.  The
+     * argument will indicate why the request is being
+     * cancelled and may be "out-of-range", "user-canceled",
+     * "timed-out" or "shutdown".
+     */
+    Q_NOREPLY void Cancel(const QString &reason) {
+        qDebug() << "auth request was cancelled" << reason;
+    }
+
+    Iwd *m_iwd;
+};
+
 
 #endif // IWD_H
